00aa19ed29c7b48bd168ce982ad75d2a278feff7
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-16 14:38:27 +0530

    16th June 2024

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const app = express();
 
 const {PORT, BOOKING_PORT, AUTH_PORT} = require('./config/serverConfig');
 
-// const limiter = rateLimit({
-// 	windowMs: 2 * 60 * 1000, // Max 5 request from 1 IP in 2 min.
-// 	max: 5,
-// })
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000, // Max 5 request from 1 IP in 2 min.
+	max: 5,
+})
 
 app.use(morgan('combined'));
-// app.use(limiter);
+app.use(limiter);
 
 app.use('/bookingservice', async (req, res, next) => {
   // console.log(req.headers['x-access-token']);
